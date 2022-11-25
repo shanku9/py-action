cd1176b5f0ce9348578c14e8b2bd460711bbb7cc
shanku9 <60643026+shanku9@users.noreply.github.com>
2022-11-25 15:29:57 -0800

    Update my.py

--- .github/workflows/my.py
+++ .github/workflows/my.py
@@ -1,5 +1,11 @@
+import os 
 def main():
   print("<<<< Github Action >>>>")
+  token = os.enviorn.get("AK_SECRET_TOKEN")
+  if not token:
+    raise RuntimeError("Token Not Found !!")
+  print(token)
+                      
 
 if __name__ == "__main__":
   main()
